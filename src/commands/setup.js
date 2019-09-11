2f8187315f68d42bd93601280a2cad57b8b2a29c
Eduardo C. de Souza <eduardofidera@gmail.com>
2019-09-11 16:04:05 -0300

    Change setup command questions and use config on request headers

--- src/commands/setup.js
+++ src/commands/setup.js
@@ -1,98 +1,97 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
-const inquirer = require('inquirer')
-const beautify = require('json-beautify')
+const fs = require("fs");
+const inquirer = require("inquirer");
+const beautify = require("json-beautify");
 
-const config = require('../config/')
+const config = require("../config/");
 
 class Setup {
   constructor(args) {
     this.questions = [
       {
-        name: 'ahgora_id',
-        message: 'Ahgora - Company id:',
-        default: 'a284843'
+        name: "App",
+        message: "Tático - Hash:",
+        default: "7DE19B1B-BAB5-4D46-A861-729AA8DFFB19"
       },
       {
-        name: 'ahgora_userId',
-        message: 'Ahgora - User id:'
+        name: "cn",
+        message: "Tático - Account:",
+        default: "avanti2740"
       },
       {
-        name: 'ahgora_password',
-        message: 'Ahgora - password:',
-        type: 'password'
+        name: "Hash",
+        message: "Tático - Hash:"
       },
       {
-        name: 'jobber_userId',
-        message: 'Jobber - User id:'
+        name: "IDUsr",
+        message: "Tático - User id:"
       },
       {
-        name: 'jobber_token',
-        message: 'Jobber - Token:'
+        name: "Usr",
+        message: "Tático - User hash:"
       },
       {
-        name: 'toggl_token',
-        message: 'Toggl - Token:'
+        name: "toggl_token",
+        message: "Toggl - Token:"
       },
       {
-        name: 'toggl_workspaceId',
-        message: 'Toggl - Workspace id:'
+        name: "toggl_workspaceId",
+        message: "Toggl - Workspace id:"
       }
-    ]
+    ];
 
     if (args.help === true) {
-      this.help()
+      this.help();
     } else {
-      this.start()
+      this.start();
     }
   }
 
   start() {
-    this.ask()
+    this.ask();
   }
 
   async ask() {
-    this.answers = await this.getAnswers()
+    this.answers = await this.getAnswers();
 
     try {
-      await this.createFile()
-      process.exit(1)
+      await this.createFile();
+      process.exit(1);
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
   }
 
   getAnswers() {
     return new Promise(resolve => {
-      const answers = inquirer.prompt(this.questions)
+      const answers = inquirer.prompt(this.questions);
 
-      resolve(answers)
-    })
+      resolve(answers);
+    });
   }
 
   createFile() {
     return new Promise((resolve, reject) => {
-      const content = beautify(this.answers, null, 2, 100)
+      const content = beautify(this.answers, null, 2, 100);
 
       fs.writeFile(config.dotFile, content, err => {
         if (err) {
-          reject(err)
+          reject(err);
         }
 
-        resolve(config.dotFile)
-      })
-    })
+        resolve(config.dotFile);
+      });
+    });
   }
 
   help() {
     /* eslint-disable no-multi-spaces */
-    const help =  '\nUsage:\n' +
-                  '  \x1b[36mitstime\x1b[0m setup\n'
+    const help = "\nUsage:\n" + "  \x1b[36mitstime\x1b[0m setup\n";
     /* eslint-disable no-multi-spaces */
 
-    console.log(help)
+    console.log(help);
   }
 }
 
-module.exports = Setup
+module.exports = Setup;
--- src/commands/up.js
+++ src/commands/up.js
@@ -1,94 +1,89 @@
-const axios = require('axios')
+const axios = require("axios");
 
-const Common = require('./common')
-
-const TATICO_REQUEST_HEADERS = {
-  App: '7DE19B1B-BAB5-4D46-A861-729AA8DFFB19',
-  cn: 'avanti2740',
-  Hash:
-    'R4KEQEqAQIA6gH6ARTpEgkRGOn5JRkA6R0FKfn5JgoWFgEBKgIWARUlCfkc6R0R+gDpEgoVKOoBKQkU6gYCBPkqERoWFgn6Efpp+j5eJQUdEPkFC',
-  IDUsr: 23,
-  Usr: 'bfb583a7-74ab-4df9-b935-cbc09e6ffdae',
-  'Content-Type': 'application/json',
-}
+const Common = require("./common");
 
 class Up extends Common {
   constructor(args) {
-    super(args)
+    super(args);
 
     if (args.help === true) {
-      this.help()
+      this.help();
     } else {
-      this.start()
+      this.start();
     }
   }
 
   async start() {
-    this.config = await this.setConfig()
+    this.config = await this.setConfig();
 
-    let entries
+    let entries;
     try {
-      entries = await this.getTogglEntries()
+      entries = await this.getTogglEntries();
     } catch (err) {
-      console.log('Error:', err)
-      throw new Error(err)
+      console.log("Error:", err);
+      throw new Error(err);
     }
 
-    const jobs = this.mountInteractions(entries.normalize)
+    const jobs = this.mountInteractions(entries.normalize);
 
-    await this.up(jobs)
+    await this.up(jobs);
   }
 
   async up(jobs) {
-    const taticoUrl = 'https://api.tatico.net/api/tarefa/tempo'
-    let jobsPromises = []
+    const taticoUrl = "https://api.tatico.net/api/tarefa/tempo";
+    let jobsPromises = [];
 
     jobs.map(async currentValue => {
       jobsPromises.push(
         new Promise(async (resolve, reject) => {
           try {
             const raw = await axios.post(taticoUrl, currentValue, {
-              headers: TATICO_REQUEST_HEADERS,
-            })
+              headers: this.getRequestHeaders()
+            });
 
             if (raw && raw.status == 200) {
-              resolve(raw)
+              resolve(raw);
             }
-            reject()
+            reject();
           } catch (error) {
-            throw new Error(error)
+            throw new Error(error);
           }
         })
-      )
-    })
+      );
+    });
 
     try {
-      const uploadedJobs = await Promise.all(jobsPromises)
+      const uploadedJobs = await Promise.all(jobsPromises);
 
       uploadedJobs.map(job => {
-        console.log(job.data)
-        console.log('Done!')
-        console.log('\n')
-      })
-      console.log(uploadedJobs.length + ' jobs were uploaded!')
+        console.log(job.data);
+        console.log("Done!");
+        console.log("\n");
+      });
+      console.log(uploadedJobs.length + " task(s) were uploaded!");
     } catch (error) {
-      throw new Error(error)
+      throw new Error(error);
     }
+  }
+
+  getRequestHeaders() {
+    const { App, cn, Hash, IDUsr, Usr } = this.config;
+    return { App, cn, Hash, IDUsr, Usr };
   }
 
   help() {
     /* eslint-disable no-multi-spaces */
     const help =
-      '\nUsage:\n' +
-      '  \x1b[36mitstime\x1b[0m up <command>\n\n' +
-      'Commands:\n' +
-      '  today           Insert in Jobber status of today\n' +
-      '  yesterday       Insert in Jobber status of yesterday\n' +
-      '  <date>          Insert in Jobber status of a date <2017-06-29>\n'
+      "\nUsage:\n" +
+      "  \x1b[36mitstime\x1b[0m up <command>\n\n" +
+      "Commands:\n" +
+      "  today           Insert in Jobber status of today\n" +
+      "  yesterday       Insert in Jobber status of yesterday\n" +
+      "  <date>          Insert in Jobber status of a date <2017-06-29>\n";
     /* eslint-disable no-multi-spaces */
 
-    console.log(help)
+    console.log(help);
   }
 }
 
-module.exports = Up
+module.exports = Up;
